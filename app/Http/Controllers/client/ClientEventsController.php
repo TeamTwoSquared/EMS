92587cefc06773ce5d46b727cdf9fab47baa5773
unknown <sajunsandaruwan@gmail.com>
2018-09-01 12:59:30 +0530

    All Controllers are made
    > Controllers are made at app/HTTP/Controllers
    > Controllers are group into folders

---
+++ app/Http/Controllers/client/ClientEventsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\client;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ClientEventsController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/support/SupportRequestAttachmentsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\support;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SupportRequestAttachmentsController extends Controller
+{
+    //
+}
