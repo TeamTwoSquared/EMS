7e5756b4e17fc87ef29b7214221116f1a2bb3e25
Gayan/HT <gayansampathmanamendra@gmail.com>
2018-09-02 12:29:43 +0530

    Update loginController.php

---
+++ app/Http/Controllers/admin/loginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class loginController extends Controller
+{
+
+ 
+    public function sinIn(){
+        //
+    }
+
+    public function sinUp(){
+        //
+    }
+
+    public function forgotPassword(){
+        //
+    }
+
+    public function rememberUserNameAndPassword(){
+        //
+    }
+
+
+}
