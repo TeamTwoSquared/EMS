bbed88b59aca79ecf39246af9932885dfb7a4d9a
Buddhi <crysisat217@gmail.com>
2018-09-16 22:27:24 +0530

    update templates n catergories

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -13,7 +13,7 @@
                 {{ csrf_field() }}
                     <div class="form-group">
                         <label>Email Address</label>
-                        <input class="au-input au-input--full" type="email" name="email" placeholder="Email">
+                        <input class="au-input au-input--full" type="email" name="email" maxlength="60" placeholder="Email">
                     </div>
                     <div class="form-group">
                         <label>Password</label>
