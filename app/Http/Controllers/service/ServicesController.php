030cc1c82ae0550bfc15cb2b0e34357193952645
pansilunilaweera <42769227+pansilunilaweera@users.noreply.github.com>
2018-09-15 22:26:51 -0700

    ServicesCotroller
    ServicesController edit

--- app/Http/Controllers/service/ServicesController.php
+++ app/Http/Controllers/service/ServicesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\service;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Service;
+use App\Service;
 
 class ServicesController extends Controller
 {
@@ -17,36 +17,52 @@ class ServicesController extends Controller
 
     public function create()
     {
-        //
+        return view('svp.service');
     }
 
 
     public function store(Request $request)
     {
-        //
+        $service = new Service();
+        $service->name = $request->name;
+        $service->price = $request->price;
+        $service->description = $request->description;
+        $service->isavailable = $request->isavailable;
+        $service->service_provider_id = $request->service_provider_id;
+        $service->save();
     }
 
 
     public function show($id)
     {
-        //
+        $service = Service::find($id);
+        return $service;
     }
 
 
     public function edit($id)
     {
-        //
+        $service = Service::find($id);
+        return view('service.edit')->with('service.$service');
+
     }
 
 
     public function update(Request $request, $id)
     {
-        //
+        $service = Service::find('$id');
+        $service->name = $request->name;
+        $service->price = $request->price;
+        $service->description = $request->description;
+        $service->isavailable = $request->isavailable;
+        $service->service_provider_id = $request->service_provider_id;
+        $service->save();
     }
 
 
     public function destroy($id)
     {
-        //
+        $service = Service::find($id);
+        $service->delete();
     }
 }
