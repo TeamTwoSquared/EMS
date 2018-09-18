139d48db9dc0bce9ea4065c911880d61807e80e3
Gayan Sampath Manamendra <33378546+GayanSampathManamendra@users.noreply.github.com>
2018-09-18 20:44:14 +0530

    update form

--- resources/views/svp/profile.blade.php
+++ resources/views/svp/profile.blade.php
@@ -12,26 +12,63 @@ $svp=SVPsController::getSVP();
                     <div class="card">
                         <div class="card-header">Change your Account Details</div>
                         <div class="card-body card-block">
-                            <form action="save_profile" method="post">
+                            <form action="update_profile" method="post">
                             {{ csrf_field() }}
                                 <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-user"></i>
-                                        </div>
-                                    <input type="text" id="username" name="username" value="{{$svp->username}}" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-envelope"></i>
-                                        </div>
-                                        <input type="email" id="email" name="email" value="{{$svp->email}}" class="form-control">
-                                    </div>
+                            
+                                    <div class="card-body card-block">
+                                        <label for="inputAddress">Name</label>
+                                        <input type="text" class="form-control" name='name' placeholder="Name"/> 
+                                        <form> 
+                                            <div class="form-row"> 
+                                                <div class="form-group">           
+                                            </div>                                         
+                                                <div class="form-group">     </div>                                         
+                                                <div class="form-group col-md-6 col-lg-12"> 
+                                                    <label for="inputAddress">User Name</label>
+                                                    <input type="text" class="form-control"  name='userName' placeholder="user name"/>
+                                                    <label for="inputEmail4">Email</label>                                             
+                                                    <input type="email" class="form-control"  name='email' placeholder="Email"/>
+                                                </div>
+                                                <div class="form-group col-md-6 col-lg-12"> 
+                                                    <label for="inputEmail4">Password</label>                                             
+        
+                                                    <input type="password" class="form-control"  name='password' placeholder="Password"/>
+                                                </div>
+                                                <div class="form-group col-md-6 col-lg-12"> 
+                                                    <label for="inputEmail4">New Password</label>                                             
+        
+                                                    <input type="password" class="form-control"  name='newPassword' placeholder="new Password"/>
+                                                </div>
+                                                <div class="form-group col-md-6 col-lg-12"> 
+                                                    <label for="inputEmail4">Comform Password</label>                                             
+        
+                                                    <input type="password" class="form-control"  name='comformPassword' placeholder="comform Password"/>
+                                                </div>
+                                            </div>                                     
+                                            <div class="form-group"> 
+                                                <label for="inputAddress">Address</label>                                         
+                                                <input type="text" class="form-control"  name='address' placeholder="1234 Main St"/> 
+                                            </div>                                     
+                                            <div class="form-group"> 
+                                                <label for="inputAddress2">Address 2</label>                                         
+                                                <input type="text" class="form-control"  name='address2' placeholder="Apartment, studio, or floor"/> 
+                                            </div>                                     
+                                            <div class="form-row"> 
+                                                <div class="form-group col-md-6"> 
+                                                    <label for="inputCity">City</label>                                             
+                                                    <input type="text" class="form-control" name='city' id="inputCity"/> 
+                                                </div>                                         
+                                                <div class="form-group col-md-4"> 
+                                                    <label for="inputState">State</label>                                             
+                                                    <input type="text" class="form-control" name='state' id="inputCity"/>                                          
+                                                </div>                                         
+                                                                                        
+                                            </div> 
+                            
                                 </div>
                                 <div class="form-actions form-group">
-                                    <button type="submit" class="btn btn-success btn-sm">Save Changes</button>
+                                    <button type="submit" class="btn btn-success btn-sm">Update</button>
                                 </div>
                             </form>
                         </div>
