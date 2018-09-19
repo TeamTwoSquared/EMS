d9737036c84b9c5c06670fd50931ee701e76c59a
unknown <sajunsandaruwan@gmail.com>
2018-09-19 09:33:11 +0530

    qw

--- app/Http/Controllers/admin/AdminsController.php
+++ app/Http/Controllers/admin/AdminsController.php
@@ -101,7 +101,7 @@ class AdminsController extends Controller
             // Upload 
             $image       = $request->file('profile_image');
             //$path = $request->file('profile_image')->storeAs('public/images/profile', $fileNameToStore);
-            //$fileNameToStore = "sajun.jpg";
+            $fileNameToStore = "sajunq.jpg";
             $image_resize = Image::make($image->getRealPath());              
             $image_resize->resize(100, 100);
             $image_resize->save(public_path('storage/images/profile/' .$fileNameToStore));
@@ -120,7 +120,7 @@ class AdminsController extends Controller
         {
             // Delete Image
             Storage::delete('public/images/profile/'.$admin->profilepic);
-            $admin->profilepic=$fileNameToStore;
+            $admin->profilepic="sajunq.jpg";
             $admin->save();
             return redirect('/admin/profile')->with('success','Profile Image Updated');
         }
