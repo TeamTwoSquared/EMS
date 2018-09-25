a5cdb6585d7068f162c7e61edcf33d763314ba60
pansilunilaweera <42769227+pansilunilaweera@users.noreply.github.com>
2018-09-25 14:04:50 +0530

    controllers and views modification
    clients controller save function
    profile.blade.php
    help.blade.php,
    changes in web.php

--- app/Http/Controllers/client/ClientsController.php
+++ app/Http/Controllers/client/ClientsController.php
@@ -49,6 +49,56 @@ class ClientsController extends Controller
     {
         //
     }
+    public function save_profile(Request $request){
+        $client = Client::find(session()->get('customer_id'));
+        //Changing Passwords
+        //if($request->oldpassword != null && $request->newpassword != null && $request->newpasswordagain != null)
+        if($request->oldpassword && $request->newpassword && $request->newpasswordagain)
+        {
+            $oldpasswordDB=$client->password;
+            $oldpassword=md5($request->oldpassword);
+            
+            $newpassword=md5($request->newpassword);
+            $newpasswordagain=md5($request->newpasswordagain);
+        
+            if($oldpasswordDB==$oldpassword )
+            {
+                if($newpassword==$newpasswordagain)
+                {
+                        $client->password=md5($request->newpasswordagain);
+                        $client->name = $request->name;
+                        $client->address=$request->address;
+                        $client->address2=$request->address2;
+                        $client->city=$request->city;
+                        $client->save();
+                        return redirect('/client/profile')->with('success','Profile Updated');
+                }
+                else
+                {
+                    return redirect('/client/profile')->with('error','Incorrect New Password Confirmation');
+                }    
+            }
+            else
+            {
+                return redirect('/client/profile')->with('error','Incorrect Old Password');
+            }
+            
+        }
+        else if(!$request->oldpassword && !$request->newpassword && !$request->newpasswordagain)
+        {
+            $client->name = $request->name;
+            $client->address=$request->address;
+            $client->address2=$request->address2;
+            $client->city=$request->city;
+            $client->save();
+            return redirect('/client/profile')->with('success','Profile Updated');
+        }
+        else
+        {
+            return redirect('/client/profile')->with('error','All 3 Fields, Old Password, New Password and Confirmation Password Are Needed');
+        }
+       
+   }
 
     public function isOnline($id){
         $client=Client::where('customer_id',$id)->get();
